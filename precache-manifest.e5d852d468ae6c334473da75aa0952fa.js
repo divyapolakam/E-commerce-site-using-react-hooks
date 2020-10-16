df054b79e8c37c5b236b98e00be006649f615b28
unknown <divyapolakam24@gmail.com>
2020-10-16 15:38:38 +0530

    Updates

---
+++ precache-manifest.e5d852d468ae6c334473da75aa0952fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32034f17c615fd42dc4d",
+    "url": "/e-commerce-using-react-hooks/static/css/main.a46d9eec.chunk.css"
+  },
+  {
+    "revision": "32034f17c615fd42dc4d",
+    "url": "/e-commerce-using-react-hooks/static/js/main.32034f17.chunk.js"
+  },
+  {
+    "revision": "d7b61b01227313a84eab",
+    "url": "/e-commerce-using-react-hooks/static/js/runtime~main.d7b61b01.js"
+  },
+  {
+    "revision": "8e3938f92a9e9f5f7e70",
+    "url": "/e-commerce-using-react-hooks/static/js/2.8e3938f9.chunk.js"
+  },
+  {
+    "revision": "44bfb680faf7dca71425f7429aaed2fe",
+    "url": "/e-commerce-using-react-hooks/index.html"
+  }
+];
